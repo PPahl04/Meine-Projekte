0c9be7b0fb98d17f733b959aed0b7af29f82d480
Pamela Pahl <107649263+PPahl04@users.noreply.github.com>
2022-06-17 00:49:19 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-#lets call the user a bitch today!!!
-name= input("Enter Your Name Here:")
-mid_name= input("Enter Your Middle Name:")
-last_name= input("Enter Your Surname Here:")
-age= input("Enter Your Age Here:")
-print(f"Bro this fucker named {name } {mid_name } {last_name} is a fucking bitch lmaooo, like imagine being alive for {age} fucking years xddd.")
-print("What a fuckking loser.")
