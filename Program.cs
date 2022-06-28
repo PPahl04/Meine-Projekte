4acb09566719ae01ec7711a4c9da64728030dfc7
Pamela Pahl <107649263+PPahl04@users.noreply.github.com>
2022-06-28 13:50:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeGroup
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age.");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age <= 1)
+            {
+                Console.WriteLine("You are {0} years old, which makes you an infant.", age);
+                Console.WriteLine("What are you doing here.");
+            }
+            else if (age <= 4)
+            {
+                Console.WriteLine("You are {0} years old, which makes you a toddler.", age);
+            }
+            else if (age <= 12)
+            {
+                Console.WriteLine("You are {0} years old, which makes you a child.", age);
+            }
+            else if (age <= 17)
+            {
+                Console.WriteLine("You are {0} years old, which makes you a teenager.", age);
+            }
+            else if (age <= 19)
+            {
+                Console.Write("You are {0} years old, which makes you a teenager. You are also a young adult.", age);
+            }
+            else if (age <= 39)
+            {
+                Console.WriteLine("You are {0} years old, which makes you an adult.", age);
+            }
+            else if (age <= 59)
+            {
+                Console.WriteLine("You are {0} years old, which makes you a middle aged adult.", age);
+            }
+            else if (age <= 110)
+            {
+                Console.WriteLine("You are {0} years old, which makes you a senior.", age);
+            }
+            else if (age > 110)
+            {
+                Console.WriteLine("You are {0} years old...", age);
+                Console.WriteLine("How are you even alive???");
+            }
+
+        }
+    }
+}
